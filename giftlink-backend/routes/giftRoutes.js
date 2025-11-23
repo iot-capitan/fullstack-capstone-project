08839396d1486aaf90eb44b8a46cc10efdf07e12
iot-capitan <o.almrayat@psut.edu.jo>
2025-11-23 22:59:24 +0300

    finished the forntend part

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const connectToDatabase = require('./db');
+const connectToDatabase = require('../models/db');
 const { ObjectId } = require('mongodb');
 
 router.get('/', async (req, res) => {
