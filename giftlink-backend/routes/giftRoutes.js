aaa9ddc5849e498aaa17ac0d0eff45189183a4f7
iot-capitan <o.almrayat@psut.edu.jo>
2025-12-01 14:46:01 -0500

    minor changes to .env

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,53 +1,50 @@
 const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
-const { ObjectId } = require('mongodb');
+const logger = require('../logger');
 
-router.get('/', async (req, res) => {
+// Get all gifts
+router.get('/', async (req, res, next) => {
+    logger.info('/ called');
     try {
         const db = await connectToDatabase();
+
         const collection = db.collection("gifts");
-
         const gifts = await collection.find({}).toArray();
-
         res.json(gifts);
     } catch (e) {
-        console.error('Error fetching gifts:', e);
-        res.status(500).send('Error fetching gifts');
+        logger.console.error('oops something went wrong', e)
+        next(e);
     }
 });
 
-router.get('/:id', async (req, res) => {
+// Get a single gift by ID
+router.get('/:id', async (req, res, next) => {
     try {
         const db = await connectToDatabase();
         const collection = db.collection("gifts");
-
         const id = req.params.id;
-
-        // If you want to query by custom "id" field:
         const gift = await collection.findOne({ id: id });
 
-
         if (!gift) {
-            return res.status(404).send('Gift not found');
+            return res.status(404).send("Gift not found");
         }
 
         res.json(gift);
     } catch (e) {
-        console.error('Error fetching gift:', e);
-        res.status(500).send('Error fetching gift');
+        next(e);
     }
 });
 
+
+// Add a new gift
 router.post('/', async (req, res, next) => {
     try {
         const db = await connectToDatabase();
         const collection = db.collection("gifts");
-        const result = await collection.insertOne(req.body);
+        const gift = await collection.insertOne(req.body);
 
-        const gift = await collection.findOne({ _id: result.insertedId });
-
-        res.status(201).json(gift);
+        res.status(201).json(gift.ops[0]);
     } catch (e) {
         next(e);
     }
