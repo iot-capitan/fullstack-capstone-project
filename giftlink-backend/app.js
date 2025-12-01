05c1fd62815e51ff81c11d0d0178cb08e7947a8e
iot-capitan <o.almrayat@psut.edu.jo>
2025-12-01 16:07:55 -0500

    finished the MainPage.js

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,16 +1,27 @@
-    require('dotenv').config();
-    const express = require('express');
-    const cors = require('cors');
-    const pinoLogger = require('./logger');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const pinoLogger = require('./logger');
 
-    const connectToDatabase = require('./models/db');
-    const {loadData} = require("./util/import-mongo/index");
+const connectToDatabase = require('./models/db');
+const { loadData } = require("./util/import-mongo/index");
 
+const app = express();
+const port = process.env.PORT || 3060;
 
-    const app = express();
-…        res.send("Inside the server")
-    })
+// Middleware
+app.use(cors());
+app.use(express.json());
 
-    app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
-    });
+// Example route
+app.get("/", (req, res) => {
+  res.send("Inside the server");
+});
+
+// Start server
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server running on port ${port}`);
+});
