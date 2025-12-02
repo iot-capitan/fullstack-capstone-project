90edc99c854f0b2dc626809b21a7ff6d4cd4427f
iot-capitan <o.almrayat@psut.edu.jo>
2025-12-02 13:09:53 -0500

    finished authRoutes.js

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -1,27 +1,50 @@
+/*jshint esversion: 8 */
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
-const { loadData } = require("./util/import-mongo/index");
+const {loadData} = require("./util/import-mongo/index");
+
 
 const app = express();
-const port = process.env.PORT || 3060;
+app.use("*",cors());
+const port = 3060;
 
-// Middleware
-app.use(cors());
+// Connect to MongoDB; we just do this one time
+connectToDatabase().then(() => {
+    pinoLogger.info('Connected to DB');
+})
+    .catch((e) => console.error('Failed to connect to DB', e));
+
+
 app.use(express.json());
 
-// Example route
-app.get("/", (req, res) => {
-  res.send("Inside the server");
+// Route files
+const giftRoutes = require('./routes/giftRoutes');
+const authRoutes = require('./routes/authRoutes');
+const searchRoutes = require('./routes/searchRoutes');
+const pinoHttp = require('pino-http');
+const logger = require('./logger');
+
+app.use(pinoHttp({ logger }));
+
+// Use Routes
+app.use('/api/gifts', giftRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/search', searchRoutes);
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
 });
 
-// Start server
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
-app.listen(port, "0.0.0.0", () => {
+app.get("/",(req,res)=>{
+    res.send("Inside the server")
+})
+
+app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
